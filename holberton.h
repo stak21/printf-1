332b2dcbc4a9846a0d75646fd7be0753e715e5e7
wescottsharples <wesc@ttsharpl.es>
2018-10-21 22:57:41 +0000

    Header file containing struct and protos

--- holberton.h
+++ holberton.h
@@ -1,6 +1,18 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
+unsigned int _strlen(char *str);
+void *_buffer(char *str);
+
+typedef struct format
+{
+	char *format;
+	void (*f)(va_list);
+} format_t;
 
 #endif /* _HOLBERTON_H_ */
