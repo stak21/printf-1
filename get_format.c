2feebdbcadee95fd61b8466786afc1c2fa36df20
wescottsharples <wesc@ttsharpl.es>
2018-10-22 03:53:04 +0000

    Function for matching specifier to function

---
+++ get_format.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+int (*get_format(const char *format, char *buffer))(va_list)
+{
+	/* Variable declaration */
+	int i;
+	format_t formats[] = {
+	{"c", char_fmt},
+	{"s", str_fmt},
+	{"d", int_fmt},
+	{"i", int_fmt},
+	{NULL, NULL}
+	};
+
+	/* Variable initialization */
+	i = 0;
+
+	/* Match conversion specifier to correct function */
+	while (formats[i].format[0])
+	{
+		/* If match is found return pointer to correct function */
+		if (*format == formats[i].format[0])
+		{
+			return (formats[i].f);
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
