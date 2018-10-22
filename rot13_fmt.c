abef0993a33a84e05c01b75d54fc8362f5cff0d3
wescottsharples <wesc@ttsharpl.es>
2018-10-22 20:13:36 +0000

    Function moves rot13 encoded string to buff

---
+++ rot13_fmt.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+mk_buffer rot13_fmt(mk_buffer buffer, va_list args)
+{
+	int i;
+	char *str;
+
+	char shift13[52] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+	'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	'l', 'm',
+	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
+	'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+	char letters[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+	'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+	'y', 'z',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+	'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	str = va_arg(args, char *);
+
+	while (*str)
+	{
+		for (i = 0; letters[i]; i++)
+		{
+			if (*str == letters[i])
+			{
+				*buffer.box = shift13[i];
+				break;
+			}
+		}
+
+		buffer.box++;
+		buffer.size++;
+		str++;
+	}
+
+	buffer.box--;
+	return (buffer);
+}
