fe7af4a5901a5f3984314685f967f4d6a1094573
wescottsharples <wesc@ttsharpl.es>
2018-10-22 21:30:41 +0000

    Function converts decimal to lower hex and moves to buff

---
+++ low_hex_fmt.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+mk_buffer low_hex_fmt(mk_buffer buffer, va_list args)
+{
+	int num, temp, i, len;
+	char rev_hex[100];
+
+	num = va_arg(args, int);
+
+	len = 0;
+	while (num)
+	{
+		temp = 0;
+
+		temp = num % 16;
+
+		if (temp < 10)
+		{
+			rev_hex[len] = temp + 48;
+			len++;
+		}
+		else
+		{
+			rev_hex[len] = temp + 87;
+			len++;
+		}
+
+		num /= 16;
+	}
+
+	i = 0;
+	len--;
+	while (i <= len)
+	{
+		*buffer.box = rev_hex[len];
+
+		buffer.box++;
+		buffer.size++;
+		len--;
+	}
+
+	buffer.box--;
+	return (buffer);
+}
