628e7e6c4e74ab27805c5f7c49aa62ccfbaa659a
stak21 <stakashima21@gmail.com>
2018-10-22 21:23:59 +0000

    replaced a section with rec_digits, Created rec_digits

--- int_fmt.c
+++ int_fmt.c
@@ -3,19 +3,16 @@
 mk_buffer int_fmt(mk_buffer buffer, va_list args)
 {
 	long int num;
-
+printf("test");
 	num = va_arg(args, int);
-	if (num > 2147483647)
-	{
-		_printf("Error");
-		exit(1);
-	}
+	printf("test: %i\n", num);
 	if (num < 0)
 	{
 		*buffer.box = '-';
-		num -= num;
+		num = -num;
 		buffer.box++;
 		buffer.size += 1;
+		printf("%i\n", num);
 	}
 	else if (num == 0)
 	{
@@ -23,21 +20,22 @@ mk_buffer int_fmt(mk_buffer buffer, va_list args)
 		buffer.box++;
 		buffer.size += 1;
 	}
-
-	while (num / 10)
-	{
-		*buffer.box = (num / 10 + '0');
-		num %= 10;
-		buffer.box++;
-		buffer.size += 1;
-
-		if (num / 10 == 0)
-		{
-			*buffer.box = (num % 10 + '0');
-			buffer.box++;
-			buffer.size += 1;
-		}
-	}
+	
+	buffer = rec_digits(num, buffer);
+		
 	buffer.box--;
 	return (buffer);
+}
+mk_buffer rec_digits(int num, mk_buffer buffer)
+{
+	if (num == 0)
+		return (buffer);
+	else
+		buffer = rec_digits(num / 10, buffer);
+
+	*buffer.box = num % 10 + '0';
+	buffer.box++;
+	buffer.size += 1;
+
+	return(buffer);
 }
