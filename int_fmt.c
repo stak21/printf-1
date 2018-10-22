215e91880b7568ac8c2a02046769c821860759e5
wescottsharples <wesc@ttsharpl.es>
2018-10-22 16:50:11 +0000

    Function converts int to chars in buff

---
+++ int_fmt.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+char *int_fmt(char *buffer, va_list args, unsigned int size)
+{
+	int num;
+	unsigned int uns_num;
+
+	num = va_arg(args, int);
+
+	if (num < 0)
+	{
+		*buffer = '-';
+		num -= num;
+		buffer++;
+		size += 1;
+	}
+
+	uns_num = num;
+
+	while (num / 10)
+	{
+		*buffer = uns_num / 10 + '0';
+		num /= 10;
+		buffer++;
+		size += 1;
+
+		if (!(uns_num / 10))
+		{
+			*buffer = uns_num % 10 + '0';
+			size += 1;
+		}
+	}
+
+	return (buffer);
+}
