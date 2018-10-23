c52dc32dac810e159416867bfa363f3f9c1b7b26
wescottsharples <wesc@ttsharpl.es>
2018-10-23 18:14:50 +0000

    Change structure of printf to make use of write function

--- helper_func.c
+++ helper_func.c
@@ -13,3 +13,29 @@ mk_buffer create_buffer(mk_buffer container)
 	}
 	return (container);
 }
+
+mk_buffer add_buff(mk_buffer buff, va_list var, const char *fmt, char custom)
+{
+	char using_char = (custom ? custom : *fmt);
+	(void)var;
+	*buff.box = using_char;
+	buff.box++;
+	buff.size++;
+	return (buff);
+}
+mk_buffer nl_fmt(mk_buffer buff, va_list var)
+{
+	(void)var;
+	*buff.box = '%';
+	buff.box++;
+	*buff.box = '\n';
+	buff.box++;
+	buff.size += 2;
+	return (buff);
+}
+
+mk_buffer spc_fmt(mk_buffer buff, va_list var)
+{
+	(void)var;
+	return (buff);
+}
--- printf.c
+++ printf.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+int write_print(mk_buffer container, va_list args);
+
 /**
  * _printf - Creates a buffer and writes that buffer to standard output
  * @format: the string to be printed, may contain conversion specifiers
@@ -19,21 +21,66 @@ int _printf(const char *format, ...)
 	{
 		if (*format == '%' && get_format(format + 1))
 		{
+			if (!(get_format(format + 1)))
+			{
+				container = add_buff(container, args, format, 0);
+					format++;
+				continue;
+			}
+			while (*(format + 1) == ' ')
+				format++;
+			if (*(format + 1) == '\0')
+			{
+				format++;
+				continue;
+			}
+			else if (*(format + 1) == '\n')
+			{
+				container = add_buff(container, args, 0, '%');
+				format++;
+				continue;
+			}
+			if (*(format + 1) && !(get_format(format + 1)))
+			{
+				container = add_buff(container, args, 0, '%');
+				container = add_buff(container, args, format, 0);
+				format++;
+				continue;
+			}
+			else if (*format == ' ')
+				container = add_buff(container, args, format, 0);
 			format++;
 			container = get_format(format)(container, args);
 		}
+		else if (*format == '%' && *(format + 1) == '\0')
+		{
+			return (-1);
+		}
 		else
 		{
-			*container.box = *format;
-			container.size += 1;
 			if (*(format + 1) == '%' && *format == '%')
 				format++;
+			*container.box = *format;
+			container.size += 1;
 		}
 		container.box++;
 		format++;
 	}
+	return (write_print(container, args));
+}
+
+/**
+ * write_print - Writes and frees the buffer to standard output
+ * @container: the string to be printed, may contain conversion specifiers
+ * which placehold for other data types to be printed
+ * @args: the args
+ * Return: The number of characters printed.
+ */
+int write_print(mk_buffer container, va_list args)
+{
 	write(1, container.start, container.size);
 	free(container.start);
 	va_end(args);
+
 	return (container.size);
 }
